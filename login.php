d9cc83f0a7e5202a74696ca5065cc9404dec043d
rodrigo <jr.milo.24@gmail.com>
2024-02-20 11:49:48 -0400

    Actualizacion del login

--- login.php
+++ login.php
@@ -6,6 +6,9 @@
     <title>Formulario</title>
 </head>
 <body>
-    
+    <h1>Formulario</h1>
+    <p>Contacto</p>
+    <br>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, nam quibusdam! Tempore excepturi eaque in tenetur, accusamus at corporis ad quibusdam cupiditate magnam laboriosam nobis odio sit modi dolores fugiat.</p>
 </body>
 </html>
